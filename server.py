455bbf470b6a1ac41c3052f776e725640bb9df17
Jorakasi <gabor.olah0323@gmail.com>
2019-11-11 14:34:49 +0100

    Main User Stories

--- server.py
+++ server.py
@@ -1,1 +1,22 @@
 from flask import Flask, request,
+
+app = Flask(__name__)
+
+@app.route('/list')
+	def listing_questions():
+		pass
+
+
+@app.route('//question/<question_id>')
+	def display_question():
+		pass
+
+
+@app.route('/add-question')
+	def add_question():
+		pass
+
+
+@app.route('/question/<question_id>/new-answer')
+	def add_answer():
+		pass
