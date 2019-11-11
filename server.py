313da73f2929f4a822cde98589251f7a9c6f8dd0
Jorakasi <gabor.olah0323@gmail.com>
2019-11-11 14:52:48 +0100

    First stage of server created

--- server.py
+++ server.py
@@ -1,22 +1,34 @@
-from flask import Flask, request,
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def main_page():
+    return render_template('mainpage.html')
+
+
 @app.route('/list')
-	def listing_questions():
-		pass
+def listing_questions():
+	pass
 
 
-@app.route('//question/<question_id>')
-	def display_question():
-		pass
+@app.route('/question/<question_id>')
+def display_question():
+	pass
 
 
 @app.route('/add-question')
-	def add_question():
-		pass
+def add_question():
+	pass
 
 
 @app.route('/question/<question_id>/new-answer')
-	def add_answer():
-		pass
+def add_answer():
+	pass
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True, # Allow verbose error reports
+        port=6969 # Set custom port
+    )
