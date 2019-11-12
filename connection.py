a9ece7fb129b078dd8e174b38fc363e21f67d895
marthon-szabo <makotoimx@gmail.com>
2019-11-12 11:01:19 +0100

    add_question func for connection.py

--- connection.py
+++ connection.py
@@ -7,26 +7,26 @@ questions = []
 answer_head = ['id', 'submisson_time','vote_number','question_id','message','image']
 
 def read_answers():
-    with open('answer.csv', 'r', newline='') as csvfile:
+    with open('answer.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for record in reader:
             answers.append(record)
 
 def write_answers():
-    with open('answer.csv', 'w',newline='') as csvfile:
+    with open('answer.csv', 'w') as csvfile:
         writer = csv.DictWriter(csvfile,fieldnames=answer_head)
         writer.writeheader()
         for record in answers:
             writer.writerow(record)
 
 def read_questions():
-    with open('question.csv', 'r', newline='') as csvfile:
+    with open('answer.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for record in reader:
             questions.append(record)
 
 def write_questions():
-    with open('question.csv', 'w',newline='') as csvfile:
+    with open('question.csv', 'w') as csvfile:
         writer = csv.DictWriter(csvfile,fieldnames=question_head)
         writer.writeheader()
         for record in questions:
@@ -50,5 +50,3 @@ def get_max_question_id():
     for record in questions:
         id = record['id']
     return id
-
-add_question('asd','asd','asd')
