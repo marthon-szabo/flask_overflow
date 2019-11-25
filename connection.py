2b4aaa1eaa411e63822e3ccb4018e856c8496507
Petiazediter <pverebes74@gmail.com>
2019-11-25 10:52:29 +0100

    CONNECTION.PY fixed

--- connection.py
+++ connection.py
@@ -5,21 +5,22 @@ import psycopg2.extras
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = os.environ.get('peti')
-    password = os.environ.get('peter133')
-    host = os.environ.get('localhost')
-    database_name = os.environ.get('sql_teszt')
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    database_name = os.environ.get('PSQL_DB_NAME')
 
     env_variables_defined = user_name and password and host and database_name
-
-
         # this string describes all info for psycopg2 to connect to the database
-    return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
-        user_name='peti',
-        password='peter133',
-        host='localhost',
-        database_name='sql_teszt'
-    )
+    if env_variables_defined:
+        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+            user_name=user_name,
+            password=password,
+            host=host,
+            database_name=database_name
+        )
+    else:
+        raise KeyError("Something wrong")
 
 
 
