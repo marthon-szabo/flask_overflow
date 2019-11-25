97a17f9342cc42a4db3a9318189075be635d1ce6
Petiazediter <pverebes74@gmail.com>
2019-11-25 15:05:06 +0100

    Connection string bugg

--- connection.py
+++ connection.py
@@ -5,22 +5,22 @@ import psycopg2.extras
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    database_name = os.environ.get('PSQL_DB_NAME')
-    
+    user_name = os.environ.get('username')
+    password = os.environ.get('password')
+    host = os.environ.get('host')
+    database_name = os.environ.get('db')
+
     env_variables_defined = user_name and password and host and database_name
         # this string describes all info for psycopg2 to connect to the database
     if env_variables_defined:
         return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
-            user_name=user_name,
-            password=password,
-            host=host,
-            database_name=database_name
+            user_name='peti',
+            password='peter133',
+            host='localhost',
+            database_name='flaskoverflow'
         )
     else:
-        raise KeyError("Something wrong")
+        raise KeyError("Bugg")
 
 
 
