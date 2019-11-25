8d6adfd8a83935a7f9f82c244865598c2cbdaaa6
Petiazediter <pverebes74@gmail.com>
2019-11-25 14:38:08 +0100

    Connection fix

---
+++ data_manager.py
@@ -0,0 +1,130 @@
+import connection
+from datetime import datetime
+
+def get_timestamp():
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    return timestamp
+
+@connection.connection_handler
+def get_question(cursor,id_):
+    cursor.execute("""
+    SELECT * FROM question
+    WHERE id = %(id)s;
+    """,{'id':id_})
+    return cursor.fetchall()
+
+@connection.connection_handler
+def get_questions(cursor):
+    cursor.execute("""
+    SELECT * FROM question
+    """)
+    questions = cursor.fetchall()
+    return questions
+
+@connection.connection_handler
+def get_answers(cursor,question_id):
+    cursor.execute("""
+    SELECT * FROM answer
+    WHERE  question_id = %(id)s;
+    """,{'id':question_id})
+    return cursor.fetchall()
+
+@connection.connection_handler
+def add_question(cursor,title,message,image,submission_time):
+    cursor.execute("""
+        INSERT INTO question
+        (title, message, image, submission_time, view_number, vote_number)
+        VALUES (%(title)s,%(message)s,%(image)s,%(submission_time)s,0,0)
+    """,{'title':title, 'message':message, 'image':image, 'submission_time':submission_time}
+    )
+
+@connection.connection_handler
+def add_answer(cursor,message, image,question_id,submission_time):
+    cursor.execute("""
+        INSERT INTO answer
+        (submission_time, vote_number, question_id, message, image)
+        VALUES (%(submission_time)s, 0, %(question_id)s, %(message)s, %(image)s);
+    """,{'submission_time':submission_time,'question_id':question_id, 'message':message,'image':image})
+
+@connection.connection_handler
+def like_post(cursor,id_): #like answer
+    cursor.execute("""
+        UPDATE answer
+        SET vote_number = vote_number + 1
+        WHERE id = %(id)s;
+    """,{'id':id_})
+
+@connection.connection_handler
+def dislike_post(cursor,id_): #dislike answer
+    cursor.execute("""
+        UPDATE answer
+        SET vote_number = vote_number - 1
+        WHERE id = %(id)s
+    """, {'id':id_})
+
+
+@connection.connection_handler
+def like_question(cursor,id_):
+    cursor.execute("""
+        UPDATE question
+        SET vote_number = vote_number + 1
+        WHERE id = %(id)s;
+    """, {'id':id_})
+
+@connection.connection_handler
+def dislike_question(cursor, id_):
+    cursor.execute("""
+        UPDATE question
+        SET vote_number = vote_number - 1
+        WHERE id = %(id)s;
+    """, {'id':id_})
+
+@connection.connection_handler
+def view_question(cursor, id_):
+    cursor.execute("""
+        UPDATE question
+        SET view_number = view_number + 1
+        WHERE id = %(id)s;
+    """, {'id':id_})
+
+@connection.connection_handler
+def delete_anwser(cursor,id_):
+    cursor.execute("""
+    DELETE * FROM answer
+    WHERE id = %(id)s;
+    """, {'id': id_})
+
+@connection.connection_handler
+def delete_question(cursor, id_):
+    cursor.execute("""
+        DELETE * FROM answer
+        WHERE question_id = %(id)s
+    """, {'id':id_})
+
+    cursor.execute("""
+    DELETE * FROM comment
+    WHERE question_id = %(id)s
+    """, {'id':id_})
+
+    cursor.execute("""
+    DELETE * FROM question
+    WHERE id = %(id)s;
+    """,{'id':id_})
+
+@connection.connection_handler
+def search_table(cursor, searchtag):
+    cursor.execute("""
+        SELECT * FROM question
+        WHERE title ILIKE %(search)s;
+    """, {'search':searchtag})
+    return cursor.fetchall()
+
+@connection.connection_handler
+def edit_question(cursor, id_, title, message, image):
+    cursor.execute("""
+    UPDATE question 
+    SET title = %(title)s, message = %(message)s, image = %(image)s
+    WHERE id = %(id)s;
+    """,{'title':title, 'message':message, 'image':image, 'id':id_}
+    )
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import connection
+import data_manager
 from flask import Flask, request, render_template,redirect
 import datetime
 
@@ -11,44 +11,43 @@ def main_page():
 @app.route('/send_comment/<int:question_id>', methods=['GET','POST'])
 def send_comment(question_id):
     if request.form['my_comment'].replace(' ', '') != '':
-        connection.add_answer(request.form['my_comment'],request.form['image_link'],question_id)
+        data_manager.add_answer(request.form['my_comment'],request.form['image_link'],question_id,data_manager.get_timestamp())
     return redirect('/question/' + str(question_id) + "/1")
 
 @app.route('/vote_anwser<int:question_id>/<int:comment_id>', methods=['GET','POST'])
 def vote_anwser(question_id,comment_id):
-    connection.like_post(comment_id)
+    data_manager.like_post(comment_id)
     return redirect('/question/' + str(question_id) + "/1")
 
 @app.route('/upvote_question<int:question_id>', methods=['GET','POST'])
 def upvote_question(question_id):
-    connection.like_question(question_id)
+    data_manager.like_question(question_id)
     return redirect('/question/' + str(question_id) + "/1")
 
 @app.route('/delete_anwser<int:question_id>/<int:comment_id>', methods=['GET','POST'])
 def delete_anwser(question_id, comment_id):
-    connection.delete_anwser(comment_id)
-    #return display_question(question_id, False)
+    data_manager.delete_anwser(comment_id)
     return redirect('/question/'+str(question_id)  + "/1")
 
 @app.route('/delete_question<int:question_id>', methods=['GET','POST'])
 def delete_question(question_id):
-    connection.delete_question(question_id)
+    data_manager.delete_question(question_id)
     return listing_questions()
 
 @app.route('/devote_anwser<int:question_id>/<int:comment_id>', methods=['GET','POST'])
 def devote_anwser(question_id,comment_id):
-    connection.dislike_post(comment_id)
-    #return display_question(question_id, False)
+    data_manager.dislike_post(comment_id)
     return redirect('/question/'+str(question_id) + "/1")
 
 @app.route('/downvote_question<int:question_id>', methods=['GET','POST'])
 def downvote_question(question_id):
-    connection.dislike_question(question_id)
-    #return display_question(question_id,False)
+    data_manager.dislike_question(question_id)
     return redirect('/question/' + str(question_id) + "/1")
 
 @app.route('/list', methods=['GET','POST'])
 def listing_questions():
+    return render_template('list.html', questions = data_manager.get_questions(), selected = 'Heat')
+    """
     table = connection.questions
     sorted_by = "Heat"
     if request.method == 'POST':
@@ -59,38 +58,38 @@ def listing_questions():
         return render_template("list.html", selected=sorted_by, questions=reversed(connection.sort_dict_by_key(table, 'vote_number', True)))
     else:
         return render_template("list.html", selected=sorted_by, questions=connection.sort_dict_by_key(table, 'title', False))
+    """
 
 @app.route('/question/<int:question_id>/<plus_view>', methods=['GET','POST'])
 def display_question(question_id, plus_view="0"):
-    for record in connection.questions:
+    for record in data_manager.get_questions():
         if int(record['id']) == question_id:
             if plus_view == "0":
-                connection.view_question(question_id)
-            return render_template('display_question.html', question_id=question_id, questions = connection.questions, max_voted = connection.get_max_voted(question_id) ,anwsers = connection.sort_dict_by_key(connection.answers, "vote_number", True) )
-
+                data_manager.view_question(question_id)
+            return render_template('display_question.html', question_id=question_id, questions = data_manager.get_questions(), max_voted = 1 ,anwsers = data_manager.get_answers(question_id) )
     return redirect('/list')
 
 @app.route('/add-question', methods=['GET', 'POST'])
 def add_question():
     if request.method == "POST":
-        connection.add_question(request.form["new_question"], request.form["message"], request.form["image"])
+        #connection.add_question(request.form["new_question"], request.form["message"], request.form["image"])
+        data_manager.add_question(request.form['new_question'], request.form['message'], request.form['image'])
         return redirect("/list")
-    return render_template("add_question.html", questions = connection.questions)
+    return render_template("add_question.html", questions = data_manager.get_questions())
 
 @app.route('/edit-question/<int:question_id>', methods=["GET", 'POST'])
 def edit_question(question_id):
     if request.method == 'POST':
-        connection.edit_question(question_id, request.form["title"], request.form["message"], request.form["image"])
+        #connection.edit_question(question_id, request.form["title"], request.form["message"], request.form["image"])
+        data_manager.edit_question(question_id,request.form['title'], request.form['message'], request.form['image'])
         return redirect('/question/' + str(question_id) + "/1")
-    return render_template('edit_question.html', question_id=question_id, questions=connection.questions)
+    return render_template('edit_question.html', question_id=question_id, questions=data_manager.get_questions())
 
 @app.route('/super-secret', methods=['GET','POST'])
 def super_secret():
     return render_template("rickross.html")
 
 if __name__ == "__main__":
-    connection.read_answers()
-    connection.read_questions()
     app.run(
         debug=True, # Allow verbose error reports
         port=6969 # Set custom port
