f0d47b5ee8f1fcceb6e315299796687c096abd55
Petiazediter <pverebes74@gmail.com>
2019-12-09 13:28:42 +0100

    New database model

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -16,6 +16,7 @@ ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS fk_ques
 ALTER TABLE IF EXISTS ONLY public.tag DROP CONSTRAINT IF EXISTS pk_tag_id CASCADE;
 ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS fk_tag_id CASCADE;
 
+
 DROP TABLE IF EXISTS public.question;
 DROP SEQUENCE IF EXISTS public.question_id_seq;
 CREATE TABLE question (
@@ -25,7 +26,26 @@ CREATE TABLE question (
     vote_number integer,
     title text,
     message text,
-    image text
+    image text,
+    user_id integer
+);
+
+DROP TABLE IF EXISTS public.users;
+DROP SEQUENCE IF EXISTS public.user_id_seq;
+CREATE TABLE users (
+    id serial NOT NULL,
+    username text,
+    password text,
+    avatar text,
+    email text,
+    gender text,
+    reputation integer
+);
+
+DROP TABLE IF EXISTS public.accepted_answers
+CREATE TABLE accepted_answers(
+    question_id integer NOT NULL,
+    answer_id integer DEFAULT 0
 );
 
 DROP TABLE IF EXISTS public.answer;
@@ -36,7 +56,8 @@ CREATE TABLE answer (
     vote_number integer,
     question_id integer,
     message text,
-    image text
+    image text,
+    user_id integer
 );
 
 DROP TABLE IF EXISTS public.comment;
@@ -47,7 +68,8 @@ CREATE TABLE comment (
     answer_id integer,
     message text,
     submission_time timestamp without time zone,
-    edited_count integer
+    edited_count integer,
+    user_id integer
 );
 
 
@@ -68,6 +90,9 @@ CREATE TABLE tag (
 ALTER TABLE ONLY answer
     ADD CONSTRAINT pk_answer_id PRIMARY KEY (id);
 
+ALTER TABLE ONLY users
+    ADD CONSTRAINT pk_user_id PRIMARY KEY (id);
+
 ALTER TABLE ONLY comment
     ADD CONSTRAINT pk_comment_id PRIMARY KEY (id);
 
@@ -80,6 +105,8 @@ ALTER TABLE ONLY question_tag
 ALTER TABLE ONLY tag
     ADD CONSTRAINT pk_tag_id PRIMARY KEY (id);
 
+
+
 ALTER TABLE ONLY comment
     ADD CONSTRAINT fk_answer_id FOREIGN KEY (answer_id) REFERENCES answer(id);
 
@@ -88,6 +115,12 @@ ALTER TABLE ONLY answer
 
 ALTER TABLE ONLY question_tag
     ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id);
+
+ALTER TABLE ONLY accepted_answers
+    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id);
+
+ALTER TABLE ONLY accepted_answers
+    ADD CONSTRAINT fk_answer_id FOREIGN KEY (answer_id) REFERENCES answer(id);
 
 ALTER TABLE ONLY comment
     ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id);
@@ -112,6 +145,9 @@ SELECT pg_catalog.setval('question_id_seq', 2, true);
 INSERT INTO answer VALUES (1, '2017-04-28 16:49:00', 4, 1, 'You need to use brackets: my_list = []', NULL);
 INSERT INTO answer VALUES (2, '2017-04-25 14:42:00', 35, 1, 'Look it up in the Python docs', 'images/image2.jpg');
 SELECT pg_catalog.setval('answer_id_seq', 2, true);
+
+INSERT INTO users VALUES(1,'teszt','add','','asd@sql.hu', 'Male',0);
+SELECT pg_catalog.setval('user_id_seq', 1, true);
 
 INSERT INTO comment VALUES (1, 0, NULL, 'Please clarify the question as it is too vague!', '2017-05-01 05:49:00');
 INSERT INTO comment VALUES (2, NULL, 1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55:00');
